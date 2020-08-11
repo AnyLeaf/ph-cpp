73360ccdc4c68aba4f6bd96e7c5b047f9dc2a4fc
David-OConnor <david.alan.oconnor@gmail.com>
2020-08-10 21:00:30 -0700

    removed rdy pin from rtd

--- examples/arduino/rtd.ino
+++ examples/arduino/rtd.ino
@@ -1,13 +1,13 @@
 #include <Anyleaf.h>
 
-PhSensor ph_sensor;
+Rtd sensor;
 
 void setup(void) {
     Serial.begin(9600);
 
     // The first argument is the pin connected to the `CS` pin of the module. It can be any
     // digital GPIO pin.
-    Rtd sensor = Rtd(10, RtdType::Pt100, RtdWires::Three);  // 3-wire Pt100
+    sensor = Rtd(10, RtdType::Pt100, RtdWires::Three);  // 3-wire Pt100
     // Rtd sensor = Rtd(10, RtdType::Pt1000, RtdWires::Two);  // 2-wire Pt1000
 }
 
